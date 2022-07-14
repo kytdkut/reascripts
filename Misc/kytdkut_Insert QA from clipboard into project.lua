121234632f8f13edcac1db7247ab8a177b30cef1
kytdkut <mauroaramburu@gmail.com>
2022-07-14 09:50:55 -0300

    upd

--- Misc/kytdkut_Insert QA from clipboard into project.lua
+++ Misc/kytdkut_Insert QA from clipboard into project.lua
@@ -1,5 +1,5 @@
 -- description Insert QA from clipboard into project
--- @version 1.0
+-- @version 1.01
 -- @author kytdkut
 
 -- Sound in Words - only intended for internal use
@@ -30,9 +30,11 @@ function Main()
       _, _, pos, _, name, _ = reaper.EnumProjectMarkers(i)
       if name == filename then
         if #line_split == 2 then -- es solo QA Audio...
-          reaper.AddProjectMarker2(0, 0, pos, 0, line_split[2], -1, 0x1E16941)
-        elseif #line_split == 3 then -- o tiene CL?
-          reaper.AddProjectMarker2(0, 0, pos, 0, line_split[2] .. " // CL: " .. line_split[3], -1, 0x1912ebf)
+          reaper.AddProjectMarker2(0, 0, pos, 0, line_split[2], -1, 0x17373C2)
+        elseif #line_split == 3 then -- o tiene además CL?
+          reaper.AddProjectMarker2(0, 0, pos, 0, line_split[2] .. " // " .. line_split[3], -1, 0x1C2A873)
+        elseif #line_split == 4 then -- o tiene además CL y QA TEC?
+          reaper.AddProjectMarker2(0, 0, pos, 0, line_split[2] .. " // " .. line_split[3], -1, 0x164B499)
         end
       end
     end
